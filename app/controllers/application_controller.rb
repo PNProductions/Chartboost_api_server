8ad3ae509b248e4511882b3d3f6679f3a1b51815
rowandish <paolo.guglielmini.vr@gmail.com>
2014-10-04 02:25:37 +0900

    delete CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 end
